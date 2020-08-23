3935fa19ebd5461e17e47d5eef0ce2db71e1fc5f
Swiftly <side@outlook.kr>
2020-08-23 00:51:47 +0000

    2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-a_string = "like this"
-a_number = 3
-a_float = 3.12
-a_boolean = False
-a_none = None
-print(type(a_none))
+ilan = {
+  "name": "Ilan",
+  "age": 27,
+  "korean": True,
+  "favorite": ["hardware", "coffee"]
+}
 
+print(ilan)
+ilan["handsome"] = True
+print(ilan)
