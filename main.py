c92d292373a52640229a2a49ceecff726413d537
Swiftly <side@outlook.kr>
2020-08-28 02:14:53 +0000

    3

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def say_hello(name = "man", age = "1"):
+  return f"Hello {name}, you are {age} years old."
+
+def plus(a, b):
+  return a + b
+
 ilan = {
   "name": "Ilan",
   "age": 27,
@@ -8,3 +14,10 @@ ilan = {
 print(ilan)
 ilan["handsome"] = True
 print(ilan)
+
+print(int("18"))
+
+hello = say_hello("Ilan", "22")
+print(hello)
+
+print(plus(a = 10, b = 22))
