5ea4e3d06d19333b37d351ac6d91bafb4fd7dd51
Swiftly <side@outlook.kr>
2020-08-28 02:23:29 +0000

    4

---
+++ calculator.py
@@ -0,0 +1,17 @@
+def plus(a, b):
+  return int(a) + int(b)
+
+def minus(a, b):
+  return int(a) - int(b)
+
+def times(a, b):
+  return int(a) * int(b)
+
+def division(a, b):
+  return int(a) / int(b)
+
+def negation(a):
+  return -int(a)
+
+def power(a, b):
+  return int(a) ** int(b)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import math
+from calculator import plus, minus
+
 def say_hello(name = "man", age = "1"):
   return f"Hello {name}, you are {age} years old."
-
-def plus(a, b):
-  return a + b
 
 ilan = {
   "name": "Ilan",
@@ -20,4 +20,9 @@ print(int("18"))
 hello = say_hello("Ilan", "22")
 print(hello)
 
-print(plus(a = 10, b = 22))
+print(plus(a = "10", b = 22))
+
+print(math.ceil(1.2))
+print(math.fabs(-1.2))
+
+print(plus(1,2), minus(1,2))
