2f1c64ce8ce71a0076bf4cdc78f4acfd1e5beef3
Swiftly <side@outlook.kr>
2020-08-29 07:29:27 +0000

    5

--- main.py
+++ main.py
@@ -1,28 +1,5 @@
-import math
-from calculator import plus, minus
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-def say_hello(name = "man", age = "1"):
-  return f"Hello {name}, you are {age} years old."
+last_indeed_pages = extract_indeed_pages()
 
-ilan = {
-  "name": "Ilan",
-  "age": 27,
-  "korean": True,
-  "favorite": ["hardware", "coffee"]
-}
-
-print(ilan)
-ilan["handsome"] = True
-print(ilan)
-
-print(int("18"))
-
-hello = say_hello("Ilan", "22")
-print(hello)
-
-print(plus(a = "10", b = 22))
-
-print(math.ceil(1.2))
-print(math.fabs(-1.2))
-
-print(plus(1,2), minus(1,2))
+indeed_jobs = extract_indeed_jobs(last_indeed_pages)
