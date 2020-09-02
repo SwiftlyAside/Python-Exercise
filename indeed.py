4f5badc2ddcada9da0d64fb6bcccd3c55884d2d6
Swiftly <side@outlook.kr>
2020-09-02 02:41:19 +0000

    6

--- indeed.py
+++ indeed.py
@@ -5,7 +5,7 @@ LIMIT = 50
 URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}"
 
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pagination = soup.find("div", {"class": "pagination"})
@@ -38,7 +38,7 @@ def extract_job(html):
     }
 
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
       print(f"Scrapping page {page}")
@@ -49,3 +49,8 @@ def extract_indeed_jobs(last_page):
           job = extract_job(result)
           jobs.append(job)
     return jobs
+
+def get_jobs():
+  last_pages = get_last_page()
+  jobs = extract_jobs(last_pages)
+  return jobs
